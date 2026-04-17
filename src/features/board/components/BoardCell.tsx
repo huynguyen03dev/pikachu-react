a333c6a693d3123e04f5c0aa35c9af6868db94c6
Huy Nguyen <huy023156@gmail.com>
2026-04-17 15:07:31 +0700

    Add real pikachu images

--- src/features/board/components/BoardCell.tsx
+++ src/features/board/components/BoardCell.tsx
@@ -9,6 +9,10 @@ interface BoardCellProps {
 }
 
 export const BoardCell = React.memo(({ cell, isSelected, onClick }: BoardCellProps) => {
+  const imagePathForTileType = (tileType: number) => {
+    return `/images/${tileType}.png`;
+  }
+
   if (cell.kind === "empty") {
     return (
       <div
@@ -23,11 +27,12 @@ export const BoardCell = React.memo(({ cell, isSelected, onClick }: BoardCellPro
       type="button"
       onClick={() => onClick(cell.id)}
       className={cn(
-        "aspect-square border border-gray-400  active:bg-gray-400 transition-colors",
+        "aspect-square relative border border-gray-400  active:bg-gray-400 transition-colors",
         isSelected ? "bg-yellow-300 hover:bg-yellow-400" : "bg-gray-200 hover:bg-gray-300"
       )}
     >
-      <p>{cell.tileType}</p>
+      <img className="w-full h-full object-cover" src={imagePathForTileType(cell.tileType)} alt={`Tile ${cell.tileType}`} />
+      {isSelected && <div className="absolute inset-0 border-4 border-red-500 pointer-events-none" />}
     </button>
   )
 });
