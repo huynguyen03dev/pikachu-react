331cb69eca21d7e4e20dea541b22f8822a27125c
Huy Nguyen <huy023156@gmail.com>
2026-04-17 15:59:02 +0700

    Extract MatchLine from Board component

--- src/features/board/Board.tsx
+++ src/features/board/Board.tsx
@@ -1,75 +1,24 @@
-import { useCallback, useEffect, useRef, useState, type JSX } from "react"
+import { useCallback, useRef, type JSX } from "react"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
-import { selectCells, selectCols, selectRows, selectSelectedCellIds, selectMatchPath } from "./boardSlice"
+import { selectCells, selectSelectedCellIds } from "./boardSlice"
 import { BoardCell } from "./components/BoardCell"
 import { SELLECT_CELL } from "../../saga"
 import Dialog from "./components/Dialog"
+import MatchLine from "./components/MatchLine"
 
-interface Point {
-  x: number,
-  y: number
-}
 
-interface Line {
-  x1: number,
-  y1: number,
-  x2: number,
-  y2: number,
-}
 
 export const Board = (): JSX.Element => {
-  const [dialogState, setDialogState] = useState<null | "win" | "lose">(null);
-
   const cells = useAppSelector(selectCells); 
   const dispatch = useAppDispatch();
   const selectedIds = useAppSelector(selectSelectedCellIds);
-  const rows = useAppSelector(selectRows);
-  const cols = useAppSelector(selectCols);
-  const matchPath = useAppSelector(selectMatchPath);
-  
-  const [lineList, setLineList] = useState<Line[]>([]);
 
   const handleSelect = useCallback((cellId: number) => {
     dispatch({ type: SELLECT_CELL, payload: cellId });
   }, [dispatch]);
 
-  const boardRef = useRef<HTMLDivElement>(null);
-  const mainRef = useRef<HTMLElement>(null);
-
-  const pointToPixelCoords = (point: Point, center: Point, cellSize: number) 
-  : Point => {
-    const x1 = center.x - ((cols - 1)/2 - point.x) * cellSize;
-    const y1 = center.y - ((rows - 1)/2 - point.y) * cellSize;
-
-    return { x: x1, y: y1 };
-  }
-
-  useEffect(() => {
-    let rect = boardRef.current?.getBoundingClientRect();
-    let main = mainRef.current?.getBoundingClientRect();
-    const cellSize = rect ? rect.width / 12 : 0;
-
-    const center = {
-      x: rect ? rect.left + rect.width / 2 - main.left : 0,
-      y: rect ? rect.top + rect.height / 2 - main.top : 0,
-    }
-
-    const drawpoints = matchPath.map(point => pointToPixelCoords(point, center, cellSize));
-
-    for (let i = 1; i < drawpoints.length; i++) {
-      const line = {
-        x1: drawpoints[i-1].x,
-        y1: drawpoints[i-1].y,
-        x2: drawpoints[i].x,
-        y2: drawpoints[i].y,
-      };
-      setLineList(prev => [...prev, line]);
-    }
-
-    return () => {
-      setLineList([]);
-    }
-  }, [matchPath]);
+  const boardRef = useRef<HTMLDivElement | null>(null);
+  const mainRef = useRef<HTMLElement | null>(null);
 
   return (
     <main ref={mainRef} className="relative p-4 flex-1">
@@ -82,11 +31,7 @@ export const Board = (): JSX.Element => {
           })}
         </div>
       </div>
-      <svg className="absolute left-0 top-0 w-full h-full pointer-events-none">
-        {lineList.map((line, i) => (
-          <line key={i} {...line} stroke="red" strokeWidth="10" />
-        ))}
-      </svg>
+      <MatchLine boardRef={boardRef} mainRef={mainRef} />
     </main>
   )
 }
---
+++ src/features/board/components/MatchLine.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { selectCols, selectMatchPath, selectRows } from "../boardSlice";
+import { useAppSelector } from "../../../app/hooks";
+
+interface Point {
+  x: number,
+  y: number
+}
+
+interface Line {
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+}
+
+export default function MatchLine({ boardRef, mainRef }: { boardRef: React.RefObject<HTMLDivElement | null>; mainRef: React.RefObject<HTMLElement | null> }) {
+  const cols = useAppSelector(selectCols);
+  const rows = useAppSelector(selectRows);
+  const matchPath = useAppSelector(selectMatchPath);
+
+  const pointToPixelCoords = (point: Point, center: Point, cellSize: number)
+    : Point => {
+    const x1 = center.x - ((cols - 1) / 2 - point.x) * cellSize;
+    const y1 = center.y - ((rows - 1) / 2 - point.y) * cellSize;
+
+    return { x: x1, y: y1 };
+  }
+
+  const [lineList, setLineList] = useState<Line[]>([]);
+  const calculateBoardCenter = (boardRef: React.RefObject<HTMLElement | null>, mainRef: React.RefObject<HTMLElement | null>) => {
+    let rect = boardRef.current?.getBoundingClientRect();
+    let main = mainRef.current?.getBoundingClientRect();
+
+    return {
+      x: rect ? rect.left + rect.width / 2 - main.left : 0,
+      y: rect ? rect.top + rect.height / 2 - main.top : 0,
+    }
+  };
+
+  useEffect(() => {
+    const cellSize = boardRef.current?.getBoundingClientRect().width / cols || 0;
+    const center = calculateBoardCenter(boardRef, mainRef);
+    const drawpoints = matchPath.map(point => pointToPixelCoords(point, center, cellSize));
+
+    for (let i = 1; i < drawpoints.length; i++) {
+      const line = {
+        x1: drawpoints[i - 1].x,
+        y1: drawpoints[i - 1].y,
+        x2: drawpoints[i].x,
+        y2: drawpoints[i].y,
+      };
+      setLineList(prev => [...prev, line]);
+    }
+
+    return () => {
+      setLineList([]);
+    }
+  }, [matchPath]);
+
+  return (
+    <svg className="absolute left-0 top-0 w-full h-full pointer-events-none">
+      {lineList.map((line, i) => (
+        <line key={i} {...line} stroke="red" strokeWidth="10" />
+      ))}
+    </svg>
+  )
+}
